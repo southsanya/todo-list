fd1bd5e0c2cd454b0a23502b8388864619d1e561
southsanya <southsanya@gmail.com>
2023-11-10 22:02:27 +0200

    create react app

---
+++ git-to-do-list/src/App.js
@@ -0,0 +1,17 @@
+
+function App() {
+
+  
+
+  return (
+    <div>
+      <div className='app-wrapper'>
+        <div>To Do List</div>
+        <input placeholder='write down your task'></input>
+        <button>To List!</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
